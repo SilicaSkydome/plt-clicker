266d2a9753585908539c06ce99705c5dd0b5af2e
SilicaSkydome <SilicaSkydome@gmail.com>
2025-06-07 14:39:32 +0300

    07.06.25

--- src/components/Common/SessionBlocked/SessionBlocked.tsx
+++ src/components/Common/SessionBlocked/SessionBlocked.tsx
@@ -4,8 +4,8 @@ import "./SessionBlocked.css"; // Создадим стили отдельно
 const SessionBlocked: React.FC = () => {
   return (
     <div className="session-blocked">
-      <h2>Сессия заблокирована</h2>
-      <p>Пожалуйста, закройте другие вкладки с приложением.</p>
+      <h2>Session blocked</h2>
+      <p>Please close other tabs with the application.</p>
     </div>
   );
 };
