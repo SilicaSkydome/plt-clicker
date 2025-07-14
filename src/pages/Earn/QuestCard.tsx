28239f4d03f23aebb393dd8a14b3ae3756ad1d3a
SilicaSkydome <SilicaSkydome@gmail.com>
2025-07-14 14:58:12 +0300

    14.07.25 v3

--- src/pages/Earn/QuestCard.tsx
+++ src/pages/Earn/QuestCard.tsx
@@ -1,6 +1,7 @@
 // pages/Earn/QuestCard.tsx
 import React from "react";
 import { Task } from "../../Interfaces";
+import "./QuestCard.css";
 
 interface QuestCardProps {
   task: Task;
@@ -8,19 +9,23 @@ interface QuestCardProps {
 }
 
 const QuestCard: React.FC<QuestCardProps> = ({ task, onClick }) => {
+  console.log(task);
   return (
-    <div
-      className={`questCard ${task.completed ? "completed" : "active"}`}
-      onClick={onClick}
-    >
-      <img src={task.icon} alt={task.title} className="questIcon" />
-      <div className="questDetails">
-        <h3 className="questTitle">{task.title}</h3>
-        <p className="questDesc">{task.description}</p>
-        <span className="questPoints">+{task.points} pts</span>
+    <div className={`questCard ${task.completed ? "completed" : "active"}`}>
+      <div className="questContent">
+        <img src={task.icon} alt={task.title} className="questIcon" />
+        <div className="questInfo">
+          <h3 className="questTitle">{task.title}</h3>
+          <p className="questDesc">{task.description}</p>
+          <span className="questPoints">+{task.points} pts</span>
+        </div>
       </div>
-      <button className="questBtn" disabled={task.completed}>
-        {task.completed ? "Done" : task.button}
+      <button
+        className="questButton"
+        onClick={onClick}
+        disabled={task.completed}
+      >
+        {task.completed ? "Done" : task.description}
       </button>
     </div>
   );
